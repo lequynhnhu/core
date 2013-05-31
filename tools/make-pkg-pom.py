df9aaa9d25b378ec4d85536edb67ce0776e2e8ad
stania (Won-gune Hwang) <stania.pe.kr@gmail.com>
2013-06-01 04:23:02 +0900

    modified make-pkg.py to use dep files in local maven repository.

--- tools/make-pkg-pom.py
+++ tools/make-pkg-pom.py
@@ -1,4 +1,4 @@
-import sys, os, re, getopt, glob
+import sys, os, re, getopt
 from zipfile import ZipFile
 import xml.etree.ElementTree as ET
 import tempfile
@@ -7,8 +7,6 @@ from threading import Thread
 # external library (pip install plumbum)
 from plumbum import local, ProcessExecutionError, local_machine, FG, BG
 from plumbum.utils import delete
-# from setuptools (pip install setuptools)
-from distutils.version import LooseVersion
 # in bundle.py
 from bundle import Bundle, Artifact
 
@@ -86,27 +84,14 @@ def make_local_jar(gid, aid, ver):
     bundle_dir = os.path.join(repo_dir, gid.replace(".", os.path.sep), aid)
     return os.path.join(bundle_dir, ver, aid + "-" + ver + ".jar")
 
-def get_latest(group_id, arti_id):
-    user_dir = get_user_dir()
-    repo_dir = get_repo_dir(user_dir)
-
-    bundle_dir = os.path.join(repo_dir, group_id.replace(".", os.path.sep), arti_id)
-    versions = []
-    for dir in glob.glob(bundle_dir + "/*"):
-        if os.path.isdir(dir):
-            versions.append(os.path.basename(dir))
-    versions = sorted(versions, key=LooseVersion, reverse=True)
-    return bundle_dir, versions[0]
-
-def get_dep(gid, aid, ver):
-    user_dir = get_user_dir()
-    repo_dir = get_repo_dir(user_dir)
-
-    bundle_dir = os.path.join(repo_dir, gid.replace(".", os.path.sep), aid)
-    return os.path.join(bundle_dir, ver, aid + "-" + ver + ".dep")
-
-def get_bundle(gid, aid, ver):
-    return Bundle(gid, aid, None, ver, "compile", make_local_jar(gid, aid, ver))
+def get_bundle(pom):
+    if debug:
+        print pom
+    dom = ET.parse(pom)
+    groupId = get_group_id(dom)
+    artifactId = get_dom_text(dom, "./mvn:artifactId", 0)
+    version = get_dom_text(dom, "./mvn:version", 0)
+    return Bundle(groupId, artifactId, None, version, "compile", make_local_jar(groupId, artifactId, version))
 
 def read_jar(jar):
     symline = None
@@ -167,10 +152,11 @@ if __name__ == "__main__":
         print str(err)
         sys.exit(2)
 
-    leaf_bundle_strs = []
+    target_poms = []
     skip_make_dep = False
     outputfile = None
     output = sys.stdout
+    clean = False
     make_parent = False
     thread_pool_size = 4
 
@@ -178,13 +164,15 @@ if __name__ == "__main__":
         if opt[0] == "-f":
             for line in open(opt[1]):
                 if line.strip():
-                    leaf_bundle_strs.append(line.strip())
+                    target_poms.append(line.strip())
         if opt[0] == "-s":
             skip_make_dep = True
         if opt[0] == "-o":
             outputfile = opt[1]
         if opt[0] == "-d":
             debug = True
+        if opt[0] == "-c":
+            clean = True
         if opt[0] == "-p":
             make_parent = True
         if opt[0] == "-j":
@@ -201,22 +189,31 @@ if __name__ == "__main__":
         output = open(outputfile, "w")
 
     for arg in args:
-        leaf_bundle_strs.append(arg.split(":"))
-
-    leaf_bundles = [x.split(":") for x in leaf_bundle_strs]
-
-    for lb in leaf_bundles:
-        if len(lb) == 2:
-            lb.append(get_latest(lb[0], lb[1])[1])
+        target_poms.append(arg)
 
     outfiles = []
     outcnt = 0
     artifacts = {}
 
-    for lb in leaf_bundles:
-        outfiles.append(get_dep(lb[0], lb[1], lb[2]))
-        bundle = get_bundle(lb[0], lb[1], lb[2])
+    if clean:
+        for pom in target_poms:
+            outfile = os.path.join(tempfile.gettempdir(), "araqne-dep-" + str(outcnt) + ".out")
+            delete(outfile)
+            outcnt += 1
+        sys.exit(0)
+
+    futures = []
+    for pom in target_poms:
+        outfile = os.path.join(tempfile.gettempdir(), "araqne-dep-" + str(outcnt) + ".out")
+        outfiles.append(outfile)
+        bundle = get_bundle(pom)
         artifacts[bundle.artifact] = bundle
+        if not skip_make_dep:
+            q.put((make_dep, (pom, outfile)))
+        outcnt += 1
+
+    if not skip_make_dep:
+        q.join()
 
     for outfile in outfiles:
         for line in open(outfile):
