dadebb9b9ad783384a4e921cea4834b69e327b46
xeraph <xeraph@nchovy.com>
2013-06-07 17:22:42 +0900

    fixed araqne/logpresso.com#13. fixed mac osx, secure crt save/restore cursor fail bug

--- araqne-api/src/main/java/org/araqne/ansicode/CursorPosCode.java
+++ araqne-api/src/main/java/org/araqne/ansicode/CursorPosCode.java
@@ -19,26 +19,18 @@ public class CursorPosCode extends AnsiEscapeCode {
 	public enum Option {
 		Save, Restore
 	}
-	
+
 	private Option option;
 
 	public CursorPosCode(Option op) {
 		this.option = op;
 	}
 
-	private String getOptionCode() {
-		switch (option) {
-		case Save:
-			return "s";
-		case Restore:
-			return "u";
-		}
-
-		throw new RuntimeException("Invalid option code. not reachable");
-	}
-
 	@Override
 	public byte[] toByteArray() {
-		return wrapCSI(getOptionCode());
+		if (option == Option.Save)
+			return new byte[] { (byte) 0x1b, 0x37 };
+		else
+			return new byte[] { (byte) 0x1b, 0x38 };
 	}
 }
